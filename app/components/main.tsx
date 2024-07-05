0af104991096ff956d525a7ac8ce1bb2f2350ae8
rinnegard <victor.rinnegard@hotmail.com>
2024-07-05 14:07:28 +0200

    Added new favicon

--- app/components/main.tsx
+++ app/components/main.tsx
@@ -4,7 +4,7 @@ type MainProps = {
 
 export default function Main({ children }: MainProps) {
     return (
-        <main className="flex-grow flex-shrink-0 basis-auto p-3">
+        <main className="container flex-grow flex-shrink-0 basis-auto p-3">
             {children}
         </main>
     );
