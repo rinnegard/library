1247c520a56a2d3b2d1e30fd1872c00226ef3c1b
rinnegard <victor.rinnegard@hotmail.com>
2024-07-22 08:54:28 +0200

    Added image to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 import Main from "./components/main";
+import Image from "next/image";
 
 export default function Home() {
     return (
         <Main>
             <h1 className="text-3xl text-center">Welcome to the library!</h1>
+
+            <Image
+                className="mx-auto w-full"
+                src="/library.jpg"
+                alt=""
+                width="700"
+                height="700"
+                quality={80}
+            />
         </Main>
     );
 }
